6f68c5e5f6caf828f871266f5951d2abd6bfaeff
William Fischdick <jwfbit@hotmail.com>
2023-10-24 20:02:03 -0300

    :construction: Criando as variáveis dos pontos, zerando os pontos e escondendo o dado.

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const elementScore0 = document.getElementById("score--0");
+const elementScore1 = document.getElementById("score--1");
+const elementDice = document.querySelector(".dice");
+
+elementScore0.textContent = 0;
+elementScore1.textContent = 0;
+elementDice.classList.add("hidden");
