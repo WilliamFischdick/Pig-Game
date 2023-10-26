0afe95d92c4b5b7076ecc30e7c42ddda0762ef18
William Fischdick <jwfbit@hotmail.com>
2023-10-26 19:54:58 -0300

    :construction: Implementando o botão de rolar o dado e somar os pontos.

--- script.js
+++ script.js
@@ -2,10 +2,14 @@
 
 const elementScore0 = document.getElementById("score--0");
 const elementScore1 = document.getElementById("score--1");
+const elementCurrent0 = document.getElementById("current--0");
+const elementCurrent1 = document.getElementById("current--1");
 const elementDice = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
+
+let currentScore = 0;
 
 elementScore0.textContent = 0;
 elementScore1.textContent = 0;
@@ -18,4 +22,15 @@ btnRoll.addEventListener("click", function() {
 
     elementDice.classList.remove("hidden");
     elementDice.src = `dice-${dice}.png`;
+
+    if (dice !==1 ) {
+        currentScore += dice;
+        elementCurrent0.textContent = currentScore;
+    } else {
+
+    }
+
+
+
+
 })
