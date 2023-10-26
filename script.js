f9f4725c23aaa0955cd96c773252ed7cb30c1bfa
William Fischdick <jwfbit@hotmail.com>
2023-10-25 21:41:45 -0300

    :construction: Implementando o rolar dos dados.

--- script.js
+++ script.js
@@ -3,7 +3,19 @@
 const elementScore0 = document.getElementById("score--0");
 const elementScore1 = document.getElementById("score--1");
 const elementDice = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnRoll = document.querySelector(".btn--roll");
+const btnHold = document.querySelector(".btn--hold");
 
 elementScore0.textContent = 0;
 elementScore1.textContent = 0;
 elementDice.classList.add("hidden");
+
+
+btnRoll.addEventListener("click", function() {
+
+    const dice = Math.trunc(Math.random() * 6) +1;
+
+    elementDice.classList.remove("hidden");
+    elementDice.src = `dice-${dice}.png`;
+})
